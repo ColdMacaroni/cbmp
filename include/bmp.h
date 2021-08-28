258c62a57472e227ed8e89570b7c55acfa5e387d
muffin <diegoxe7117@gmail.com>
2021-08-28 21:04:17 +1200

    include stddef for size_t, header now working

--- include/bmp.h
+++ include/bmp.h
@@ -1,8 +1,9 @@
 #ifndef BMP_PROTECT
 #define BMP_PROTECT 1
+#include <stddef.h>
 #include <stdbool.h>
 
 size_t
-write_bmp(char *restrict filename, const bool *arr, const int width, const int height);
+write_bmp_1bit(char *restrict filename, const bool *arr, const int width, const int height);
 
 #endif
