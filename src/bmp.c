01a829be40c2f79cf6195b0aba0224648e10e3be
muffin <diegoxe7117@gmail.com>
2021-08-28 19:33:07 +1200

    start on writing header

--- src/bmp.c
+++ src/bmp.c
@@ -4,13 +4,65 @@
 #include <string.h>
 
 char
-*create_bmp(const bool *arr, const int width, const int height)
+*create_bmp_header(size_t *nmb, unsigned int nmb_dib, unsigned int nmb_data)
 {
+    // Bitmap header has a fixed size of 14 bytes, one more for null
+    enum { nmb_header = 14 };
+    char *header = calloc(sizeof(char), nmb_header + 1);
+    memset(header, '\0', nmb_header + 1);
+
+    /* https://en.wikipedia.org/wiki/BMP_file_format#Bitmap_file_header */
+    // ID. 2 bytes. 0-1
+    header[0] = 'B';
+    header[1] = 'M';
+
+    // Size. 4 bytes. 2-5
+    unsigned int bytes = nmb_dib + nmb_data + nmb_header;
+    *(int *)&header[2] = (int)bytes;
+
+    // Reserved. 4 bytes. 6-9
+
+    *nmb += nmb_header;
+
+    return header;
+}
+
+/* To spread an int into over multiple bytes
+ *
+ * Int. 4 bytes. Will reset leftover bits.
+ *    *(int *)&content[0] = (int)0x4e494345;
+ *    4e494345 -> 4e 49 43 45 -> 'N' 'I' 'C' 'E'
+ *    content[0] == 'E'
+ *    content[1] == 'C'
+ *    content[2] == 'I'
+ *    content[3] == 'N'
+ *
+ * Short int. 2 bytes. Will reset leftover bits.
+ *    *(short int *)&content[0] = (short int)0x4849;
+ *    4849 -> 48 49 -> 'H' 'I'
+ *    content[0] == 'I'
+ *    content[1] == 'H'
+ */
+
+char
+*create_bmp(size_t *nmb, const bool *arr, const int width, const int height)
+{
+    /*
     char *content = calloc(sizeof(char), 20);
+    memset(content, 0, 20);
 
-    content = "balls";
 
-    return content;
+    *(int *)&content[0] = (int)0x4e494345;
+
+    printf("%c\n", content[0]);
+    printf("%c\n", content[1]);
+    printf("%c\n", content[2]);
+    printf("%c\n", content[3]);
+    //*/
+
+    char *header = create_bmp_header(nmb, 40, 16);
+
+    return header;
 }
 
 // Assuming char is 4 bits,
@@ -23,9 +75,11 @@ write_bmp(char *restrict filename, const bool *arr, const int width, const int h
 {
     // Code
     FILE *file = fopen(filename, "wb+");
-    char *bmp_content = create_bmp(arr, width, height);
+    size_t nmb = 0;
 
-    size_t fwrite_result = fwrite(bmp_content, sizeof(char), strlen(bmp_content), file);
+    char *bmp_content = create_bmp(&nmb, arr, width, height);
+
+    size_t fwrite_result = fwrite(bmp_content, sizeof(char), nmb, file);
 
     free(bmp_content);
     fclose(file);
