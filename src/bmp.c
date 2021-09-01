dfbceab10f8c8311e7d41ef570cf1cabfc3e378b
muffin <diegoxe7117@gmail.com>
2021-09-02 02:42:17 +1200

    dib working

--- src/bmp.c
+++ src/bmp.c
@@ -72,21 +72,58 @@ create_bmp_header(size_t *nmb, size_t nmb_dib, size_t nmb_data)
     return header;
 }
 
+dib_t
+create_bmp_dib(size_t *nmb, size_t nmb_data,
+               int32_t width, int32_t height,
+               uint16_t bit_depth)
+{
+    dib_t dib;
+    enum {nmb_dib = 40, res = 2835};
+
+    assert(sizeof(dib_t) == nmb_dib);
+    dib.dib_size = nmb_dib;
+
+    dib.width = width;
+    dib.height = height;
+
+    dib.color_planes = 1;
+    dib.bit_depth = bit_depth;
+
+    // Not even gonna try to implement this
+    dib.compression = 0;
+
+    dib.data_size = nmb_data;
+
+    dib.h_res = res;
+    dib.v_res = res;
+
+    // no idea what these are
+    dib.color_palette = 0;
+    dib.important_colors = 0;
+
+    *nmb += nmb_dib;
+
+    return dib;
+}
 
 size_t
-write_bmp_1bit(char *restrict filename, const bool *arr, const int width, const int height)
+write_bmp_1bit(char *restrict filename, const bool *arr, int32_t width, int32_t height)
 {
     // Code
     FILE *file = fopen(filename, "wb+");
-    size_t nmb = 0;
-
+    size_t nmb_header = 0;
+    size_t nmb_dib = 0;
+    size_t nmb_data = 0;
+    size_t result = 0;
     // int8_t *bmp_content = create_bmp_1bit(&nmb, arr, width, height);
 
-    header_t header = create_bmp_header(&nmb, 40, 16);
+    dib_t dib = create_bmp_dib(&nmb_dib, nmb_data, width, height, 1);
+    header_t header = create_bmp_header(&nmb_header, nmb_dib, nmb_data);
 
-    size_t fwrite_result = fwrite(&header, 1, nmb, file);
+    result += fwrite(&header, 1, nmb_header, file);
+    result += fwrite(&dib, 1, nmb_dib, file);
 
     fclose(file);
 
-    return fwrite_result;
+    return result;
 }
