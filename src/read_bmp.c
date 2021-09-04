0a3083758c46775d042d7599838918e18647b17c
muffin <diegoxe7117@gmail.com>
2021-09-04 23:12:32 +1200

    checking header and dib size

--- src/read_bmp.c
+++ src/read_bmp.c
@@ -4,24 +4,43 @@
 
 #include "bmp_structs.h"
 
+#define cleanup(file) do {fclose(file);return NULL;} while(0)
 rgb_t
 *read_bmp(const char *restrict filename, int32_t *width, int32_t *height)
 {
-    rgb_t *arr = NULL;
-    header_t header;
+    /* Only bmp supported is BM header, BITMAPINFOHEADER dib, 24bit rgb data */
+    enum {header_nmb = 14, dib_nmb = 40};
 
     FILE *file = fopen(filename, "rb");
+    if (file == NULL)
+        return NULL;
 
-    // Get the bmp header, itll always be 14 bytes.
-    fread(&header, 1, 14, file);
+    rgb_t *arr = NULL;
 
-    // Check that it is in fact a bitmap. Or at least it says it is.
+    // Get the bmp header
+    header_t header;
+    fread(&header, 1, header_nmb, file);
+
+    // Check that it is in fact a bitmap. Or at least that says it is.
     if (header.header_field[0] != 'B' || header.header_field[1] != 'M')
     {
-        fprintf(stderr, "File format not supported\n");
-        return NULL;
+        fprintf(stderr, "File format not supported. Header field specifies %c%c\n",
+                header.header_field[0], header.header_field[1]);
+
+        cleanup(file);
     }
 
+    // Get the dib, only BITMAPINFOHEADER as of now
+    dib_t dib;
+    fread(&dib, 1, dib_nmb, file);
+
+    if (dib.dib_size != dib_nmb)
+    {
+        fprintf(stderr, "Unknown DIB header. Not supported. Size = %d\n", dib.dib_size);
+
+        cleanup(file);
+    }
 
     return arr;
-};
+}
+#undef cleanup
